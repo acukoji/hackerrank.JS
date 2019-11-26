19353e6d066236ee46c05408478c345f8e56100a
Acukoji <acukoji@gmail.com>
2019-11-26 13:27:46 -0700

    completed soln to hr.Compare.Triplets.js

--- hr.Compare.Triplets.js
+++ hr.Compare.Triplets.js
@@ -8,10 +8,13 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
+//signals input
 process.stdin.on('data', function(inputStdin) {
+    //inputStdin = "5 6 7\n3 6 10"
+    inputStdin = "17 28 30\n99 16 8"
     inputString += inputStdin;
 });
-
+//signals end of data input
 process.stdin.on('end', function() {
     inputString = inputString.split('\n');
 
@@ -22,28 +25,51 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+// Example input
+// "5 6 7\n3 6 10"
+// 17 28 30\n99 16 8
+
 // Complete the compareTriplets function below.
 function compareTriplets(a, b) {
+// degugging
+    //console.log("a= "+a);
+    // console.log(b);
+
 
     // point tracker
     var totalPointsA = 0;
     var totalPointsB = 0;
-
-    // constraints: a/b must be >=1 and <=100
-
+    var i = 0;
+    for (i; i < a.length; i++){
+        if (a[i] > b[i]){
+            //a gets point
+            totalPointsA += 1
+            //console.log('point for A')
+        } else if (b[i] > a[i]){
+            //b gets point
+            totalPointsB += 1
+            //console.log('point for B')
+        } else {
+            //no points
+            totalPointsA = totalPointsA
+            totalPointsB = totalPointsB
+            //console.log('No points: a = b')
+        }
+    }  
+        
     // comparing a with b
-    if(a > b){
-        //a gets point
-        totalPointsA ++
-    } else if(b > a){
-        //b gets point
-        totalPointsB ++
-    } else {
-        //no points
-        totalPointA = totalPointA + 0;
-        totalPointsB = totalPointsB + 0;
-    }
-    return totalPointsA, totalPointsB;
+    // if(a > b){
+    //     //a gets point
+    //     totalPointsA ++
+    // } else if(b > a){
+    //     //b gets point
+    //     totalPointsB ++
+    // } else {
+    //     //no points
+    //     totalPointA = totalPointA + 0;
+    //     totalPointsB = totalPointsB + 0;
+    //}
+    return [totalPointsA, totalPointsB];
 }
 
 function main() {
@@ -59,3 +85,16 @@ function main() {
 
     ws.end();
 }
+
+/*
+How to run this code locally from terminal.
+$ OUTPUT_PATH=/dev/fd/0 node hr.Compare.Triplets.js
+"5 6 7\n3 6 10"
+CNTRL D (to "end")
+Note: inputStdin must be passed with example data:
+e.g. 
+process.stdin.on('data', function(inputStdin) {
+    inputStdin = "5 6 7\n3 6 10"
+        or,
+    inputStdin = "17 28 30\n99 16 8"
+*/
