c74df862dbdb2a74580c903f30de996418ed9687
Acukoji <acukoji@gmail.com>
2019-11-26 18:38:58 -0700

    final soln hr.CompareTriplets.js.

--- hr.Compare.Triplets.js
+++ hr.Compare.Triplets.js
@@ -10,8 +10,6 @@ let currentLine = 0;
 
 //signals input
 process.stdin.on('data', function(inputStdin) {
-    //inputStdin = "5 6 7\n3 6 10"
-    inputStdin = "17 28 30\n99 16 8"
     inputString += inputStdin;
 });
 //signals end of data input
@@ -31,46 +29,24 @@ function readLine() {
 
 // Complete the compareTriplets function below.
 function compareTriplets(a, b) {
-// degugging
-    //console.log("a= "+a);
-    // console.log(b);
-
-
-    // point tracker
+    //point tracker
     var totalPointsA = 0;
     var totalPointsB = 0;
-    var i = 0;
-    for (i; i < a.length; i++){
+
+    for (var i=0; i < a.length; i++){
         if (a[i] > b[i]){
             //a gets point
             totalPointsA += 1
-            //console.log('point for A')
-        } else if (b[i] > a[i]){
+        } 
+        
+        if (b[i] > a[i]){
             //b gets point
             totalPointsB += 1
-            //console.log('point for B')
-        } else {
-            //no points
-            totalPointsA = totalPointsA
-            totalPointsB = totalPointsB
-            //console.log('No points: a = b')
         }
-    }  
-        
-    // comparing a with b
-    // if(a > b){
-    //     //a gets point
-    //     totalPointsA ++
-    // } else if(b > a){
-    //     //b gets point
-    //     totalPointsB ++
-    // } else {
-    //     //no points
-    //     totalPointA = totalPointA + 0;
-    //     totalPointsB = totalPointsB + 0;
-    //}
+    }
     return [totalPointsA, totalPointsB];
 }
+ 
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
@@ -89,12 +65,8 @@ function main() {
 /*
 How to run this code locally from terminal.
 $ OUTPUT_PATH=/dev/fd/0 node hr.Compare.Triplets.js
-"5 6 7\n3 6 10"
+"5 6 7
+3 6 10"
 CNTRL D (to "end")
-Note: inputStdin must be passed with example data:
-e.g. 
-process.stdin.on('data', function(inputStdin) {
-    inputStdin = "5 6 7\n3 6 10"
-        or,
-    inputStdin = "17 28 30\n99 16 8"
+
 */
